4c3e609743998b3a8e021f25f3e45175dd9bd8a5
sikari <ashish.adhikari@bison.howard.edu>
2018-04-17 18:18:23 -0400

    two implementations remaining

--- simulation.py
+++ simulation.py
@@ -30,33 +30,48 @@ def SSTF_simul(req,head):
 			min_distance = abs(cur_head - max(req))
 			max_distance = max(req)
 	return distance_moved
-
-# SCAN algorithm
+#SCAN algorithm
 def SCAN_simul(req,head):
 	cur_head = head
-	req = sorted(list(set(req)))
-	req_size = len(req)
 	distance_moved = 0
-	cur_track = min(req,key=lambda x: abs(cur_head - x))
-	#if initial direction is towards right
-	start_index = req.index(cur_track)
-	distance_moved += abs(cur_head - cur_track)
-	if cur_track >= cur_head and req_size!=0:
-		#serve towards right
-		for ix in range(start_index,req_size):
-			 cur_head = req[ix-1]
-			 cur_track = req[ix]
-		#serve towards left
-		cur_head = req[-1]
-		cur_tack = req[0]
-		distance_moved+=abs(cur_track-cur_head)
-		for ix in range(1,start_index):
-			cur_head = req[ix-1]
-			cur_track = req[ix]
-			distance_moved+= abs(cur_track-cur_head)
-	else:
-		pass
+	max_request = max(req)
+	req = sorted(req)
+	end = 4999
+	for check in range(cur_head, max_request+1):
+		if (check in req):
+			distance_moved+=abs(check-cur_head)
+			req.remove(check)
+			cur_head = check
+	#move to end
+	distance_moved += abs(cur_head - end)
+	cur_head = end
+	while max_request >= 0:
+		if (max_request in req):
+			distance_moved += abs(cur_head- max_request)
+			req.remove(max_request)
+			cur_head = max_request
+		max_request -= 1
 	return distance_moved
+
+def LOOK_simul(req,head):
+	cur_head = head
+	distance_moved = 0	
+	max_request = max(req)
+	for i in range(cur_head, max_request+1):
+		if (i in req):
+			distance_moved += abs(cur_head - i)
+			cur_head = i
+			req.remove(i)
+	while max_request >= 0:
+		if (max_request in req):
+			distance_moved += abs(cur_head - max_request)
+			cur_head = max_request
+			req.remove(max_request)
+		max_request -= 1
+	return distance_moved
+
+
+
 
 SSTF_simul([2069, 1212, 2296, 2800, 544, 1618, 356, 1523, 4965, 3681],2150)
 FCFS_simul([2069, 1212, 2296, 2800, 544, 1618, 356, 1523, 4965, 3681],2150)
